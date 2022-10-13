7216b8175108868fd6c2ba5480a1d575069b37e7
ManuelGuerraRuiz <mguerui@adaits.es>
2022-10-13 20:42:43 +0200

    13/10/2022

---
+++ T1/06-nodejs-native/Imprimir.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+
+const filename = process.argv[2];
+
+try{
+    const data = fs.readFileSync('./'+filename, 'utf8');
+    console.log(data);
+} catch (err) {
+    console.error(err);
+}
